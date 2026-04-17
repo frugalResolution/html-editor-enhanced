dc11adfbc13aaf1510c424ee0215e364e9e69c5d
Felix <frugalResolution@gmail.com>
2026-04-18 00:36:35 +0200

    gitignore android build files

--- example/android/app/src/main/kotlin/com/tanayneotia/html_editor_enhanced_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/tanayneotia/html_editor_enhanced_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.tanayneotia.html_editor_enhanced_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
